dec7e6b700581d3f58dbf44b3e024aaeb57ee4b8
HyperRushNet <hyperrushnetwork@gmail.com>
2026-03-29 19:20:12 +0200

    Add/Update 1v1-lol [19:20:12]

--- 1v1-lol/1firebase.js
+++ 1v1-lol/1firebase.js
@@ -1,1 +1,14 @@
-Package size exceeded the configured limit of 50 MB. Try https://github.com/un-zynq/g1/tree/master/1v1-lol/1firebase.js instead.
+function initializeFireBase(){
+	// Your web app's Firebase configuration
+	var firebaseConfig = {
+	apiKey: "AIzaSyBPrAfspM9RFxuNuDtSyaOZ5YRjDBNiq5I",
+	authDomain: "justbuild-cdb86.firebaseapp.com",
+	databaseURL: "https://justbuild-cdb86.firebaseio.com",
+	projectId: "justbuild-cdb86",
+	storageBucket: "justbuild-cdb86.appspot.com",
+	messagingSenderId: "93158914000",
+	appId: "1:93158914000:web:e73a8b453338ab7c"
+	};
+	// Initialize Firebase
+	firebase.initializeApp(firebaseConfig);
+}
